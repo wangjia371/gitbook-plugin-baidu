2391ba47d050b35446e3d02783567c603fba257f
wangjia371 <wangjia@growingio.com>
2016-07-13 19:57:46 +0800

    Update plugin.js

--- book/plugin.js
+++ book/plugin.js
@@ -1,5 +1,16 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
-        config.baidu = config.baidu || {};
+        var baidu_gio = config.baidu_gio || {};
+        var _hmt = _hmt || [];"
+                    + "(function() {"
+                    + "var hm = document.createElement('script');"
+                    + "hm.src = '//hm.baidu.com/hm.js?" + baidu_gio['token'] + "';"
+                    + "var s = document.getElementsByTagName('script')[0];"
+                    + "s.parentNode.insertBefore(hm, s);"
+                    + "})();
+        
+        
+        
+        
     });
 });
